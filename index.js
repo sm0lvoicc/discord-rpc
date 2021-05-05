bcb497f6b9c4bf523023737bfb64bddd0edf8150
sm0l voicc <72088954+sm0lvoicc@users.noreply.github.com>
2021-05-05 22:53:15 +0530

    Update index.js
    added comments for better understanding

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const rpc = require('discord-rpc');
 const Discord = require('discord.js-light');
-const { token } = require('./token.json');
+const { token } = require('./token.json'); //import the token of the bot from token.json or whatever you have saved it in, the different between {} and () is that, {} is the same as token.token, if you get what i mean
 
 const bot = new Discord.Client();
 const client = new rpc.Client({transport: 'ipc'});
@@ -14,22 +14,25 @@ bot.on('ready', () => {
         client.request('SET_ACTIVITY', {
             pid: 9999,
             activity: {
-                details: 'hemlo',
-                state: `no i cannot hack your crush's instagram or fix your washing machine`,
+                details: 'hemlo', /*first text from the top of the rpc*/
+                state: `no i cannot hack your crush's instagram or fix your washing machine`, /*this is the description, the 2nd text from top*/
                 assets: {
-                    large_image: "two_yes",
-                    large_text: `this me :) not really, its wumpus`,
-                    small_image: "sm0l",
-                    small_text: `${bot.guilds.cache.size} servers`
+                    large_image: "two_yes", /*the name of big image (it should be same as you have saved in the dev portal*/
+                    large_text: `this me :) not really, its wumpus`, /*this is what will be shown when you hover your cursor over it*/
+                   //small image is optional, not really important, you can have it if you want and not have it if you don't
+                    small_image: "sm0l", /*the name of the small image (it should be same as you have saved in the dev portal*/
+                    small_text: `${bot.guilds.cache.size} servers` /*the text you want to be shown when one hovers cursor over the small image*/
                 },
+                //buttons are also optional
                 buttons: [
                     {
-                        label: "INVITE SM0L",
-                        url: "https://discord.com/api/oauth2/authorize?client_id=760426095563767818&permissions=4294967287&scope=bot%20applications.commands"
+                        label: "INVITE SM0L", /*what you want to be shown on the 1st button's text*/
+                        url: "https://discord.com/api/oauth2/authorize?client_id=760426095563767818&permissions=4294967287&scope=bot%20applications.commands" /*the link or anything where it redirects*/
                     },
+                    //you can also only just have 1 button 
                     {
-                        label: "JOIN MY SERVER",
-                        url: "https://discord.gg/PG9Ahke"
+                        label: "JOIN MY SERVER", /*text for 2nd button*/
+                        url: "https://discord.gg/PG9Ahke" /*the link or stuff*/
                     }
                 ]
             }
